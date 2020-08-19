7fae120eb1e7d59a6bab9d4eab0dac8ce522b862
KartikHinge14 <69903239+KartikHinge14@users.noreply.github.com>
2020-08-19 18:17:33 +0530

    Update upgrade.ps1

--- upgrade.ps1
+++ upgrade.ps1
@@ -1,1 +1,23 @@
 $server = get-content server.txt
+foreach($node in $server){
+
+invoke-command -cn $server -scriptblock{
+
+$uninstall = Get-ChildItem -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*, HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Get-ItemProperty | Where-Object {$_.DisplayName -like '*Java *' -or $_.DisplayName -like '*J2SE*' -or $_.DisplayName -like '*Java(TM) *' -and $_.DisplayName -ne 'Java Auto Updater' -and ($_.Publisher -like '*Oracle*' -or $_.Publisher -like '*Sun*') -and $_.Publisher -ne '' } | Select-Object uninstallstring,InstallLocation,DisplayName,DisplayVersion
+    ForEach($ustr in $Uninstall){
+        echo $ustr.DisplayName$ustr.InstallLocation================= >> c:\JavaPreviousVersion.txt
+        $ustr=$ustr.UninstallString -Replace ('msiexec.exe','') -Replace ('msiexec','') -Replace ('/I','') -Replace ('/X','') -Replace ('/q','')
+        $ustr=$ustr.trim()
+        
+      
+        Start-Process C:\Windows\System32\msiexec.exe -ArgumentList "/x $ustr /qn /norestart" -wait  
+        
+        }
+        
+        
+        
+        
+        
+        
+        
+        
